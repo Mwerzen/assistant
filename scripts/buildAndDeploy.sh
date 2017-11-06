e8144ec31c7202e803428f6814f1f9ba82f8f8ad
Mike <mwerzen@gmail.com>
2017-11-06 18:16:11 -0500

    Script

--- scripts/buildAndDeploy.sh
+++ scripts/buildAndDeploy.sh
@@ -8,3 +8,8 @@ cp target/assistant*.jar /assistant/core/assistant.jar
 chmod 744 /assistant/core/assistant.jar
 sudo service assistant-core start
 
+cd ~/projects/assistant/python
+rm -r /assistant/python/.
+cp *.py /assistant/python/.
+cp secret.txt /assistant/python/.
+nohup /assistant/python/Controller.py >> /tmp/python.out 2>&1&
