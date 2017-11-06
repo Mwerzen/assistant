879fcaf9b4f7c9d7f8e2b0d7090b0d8d3a9cba38
Mike <mwerzen@gmail.com>
2017-11-06 18:56:22 -0500

    Debug

--- scripts/buildAndDeploy.sh
+++ scripts/buildAndDeploy.sh
@@ -13,4 +13,5 @@ rm -r /assistant/python/*.py
 cp *.py /assistant/python/.
 cp secret.txt /assistant/python/.
 chmod 755 /assistant/python/*
+cd /assistant/python/
 nohup python3 /assistant/python/Controller.py >> /tmp/python.out 2>&1&
